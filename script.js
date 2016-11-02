6ee3da6dd7f514da6a428aa33edf0065d6c82445
elhanine <kh.elhanine@gmail.com>
2016-11-02 23:51:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,95 @@
-/* Draw on canvas */
+$(function() {
+// 4 images
+    var image0 = new Image();
+    image0.src = "cat1.jpg";
+    var image1 = new Image();
+    image1.src = "cat2.jpg";
+    var image2 = new Image();
+    image2.src = "cat3.jpg";
+    var image3 = new Image();
+    image3.src = "cat4.jpg";
+// array of 4 images
+    var images = new Array(image0, image1, image2, image3);
 
-var canvas = document.getElementById('drawCanvas');
-var ctx = canvas.getContext('2d');
+// global counter and canvas
+    var counter = 0, ctx, interval, calc = true;
 
-ctx.lineWidth = '3';
-ctx.lineCap = 'round';
-ctx.lineJoin = 'round';
+    var canvas = document.getElementById('drawCanvas');
+    ctx = canvas.getContext('2d');
 
-var color = 'yellowgreen';
+    images[0].onload = function () {
+        ctx.drawImage(images[0], 0, 0, canvas.width, canvas.height);
+    };
 
-canvas.addEventListener('mousedown', startDraw, false);
-canvas.addEventListener('mousemove', draw, false);
-canvas.addEventListener('mouseup', endDraw, false);
+// this is the main function
+
+// this is the function called after each timeout to draw next image
 
 
-function drawOnCanvas(color, plots) {
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(plots[0].x, plots[0].y);
+    $('#nextSlide').click(function () {
 
-    for(var i=1; i<plots.length; i++) {
-        ctx.lineTo(plots[i].x, plots[i].y);
+        if (calc) {
+            counter++;
+        }
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(images[counter], 0, 0, canvas.width, canvas.height);
+
+        calc = true;
+
+        if (counter == images.length - 1) {
+
+            counter = 0;
+            calc = false;
+        }
+
+        //Usage example:
+    });
+
+
+    /* Draw on canvas */
+
+    ctx.lineWidth = '3';
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+
+    var color = 'yellowgreen';
+
+    canvas.addEventListener('mousedown', startDraw, false);
+    canvas.addEventListener('mousemove', draw, false);
+    canvas.addEventListener('mouseup', endDraw, false);
+
+
+    function drawOnCanvas(color, plots) {
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(plots[0].x, plots[0].y);
+
+        for (var i = 1; i < plots.length; i++) {
+            ctx.lineTo(plots[i].x, plots[i].y);
+        }
+        ctx.stroke();
     }
-    ctx.stroke();
-}
 
 
-var isActive = false;
-var plots = [];
+    var isActive = false;
+    var plots = [];
 
-function draw(e) {
-    if(!isActive) return;
+    function draw(e) {
+        if (!isActive) return;
 
-    var x = e.offsetX || e.layerX - canvas.offsetLeft;
-    var y = e.offsetY || e.layerY - canvas.offsetTop;
+        var x = e.offsetX || e.layerX - canvas.offsetLeft;
+        var y = e.offsetY || e.layerY - canvas.offsetTop;
 
-    plots.push({x: x, y: y});
-    drawOnCanvas(color, plots);
-}
+        plots.push({x: x, y: y});
+        drawOnCanvas(color, plots);
+    }
 
-function startDraw(e) {
-    isActive = true;
-}
+    function startDraw(e) {
+        isActive = true;
+    }
 
-function endDraw(e) {
-    isActive = false;
-    plots = [];
-}
+    function endDraw(e) {
+        isActive = false;
+        plots = [];
+    }
+});
